9fdc2dd1b1b118c99c6891222e17b8c3e3fdb8e9
jedsadakorn2002 <166341221003-st@gmail.com>
2024-06-10 15:09:36 +0700

    EditByKimmy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // รับค่าจากผู้ใช้
 var number1 = parseFloat(prompt("ป้อนตัวเลขที่หนึ่ง:"));
-var number2 = parseFloat(prompt("ป้อนตัวเลขที่สอง:"));
+var number3 = parseFloat(prompt("ป้อนตัวเลขที่สอง:"));
 
 // คำนวณและแสดงผลลัพธ์
 var sum = number1 + number2;
