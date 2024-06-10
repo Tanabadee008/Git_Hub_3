ade22a21a5becfa21de45f1e09cf23714c728771
Tanabadee <tanabadee@gmail.com>
2024-06-10 13:48:22 +0700

    my first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// รับค่าจากผู้ใช้
+var number1 = parseFloat(prompt("ป้อนตัวเลขที่หนึ่ง:"));
+var number2 = parseFloat(prompt("ป้อนตัวเลขที่สอง:"));
+
+// คำนวณและแสดงผลลัพธ์
+var sum = number1 + number2;
+console.log("ผลบวกของ " + number1 + " กับ " + number2 + " คือ " + sum);
